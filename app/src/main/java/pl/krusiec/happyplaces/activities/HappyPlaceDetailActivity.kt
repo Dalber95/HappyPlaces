6c90bc4a43f50e68b101dc844915dc657e22a65d
Kamil <krusiec.kamil82@gmail.com>
2020-06-15 14:07:21 +0200

    Adding ActivityForResult In Order To Update The List Dynamically

---
+++ app/src/main/java/pl/krusiec/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -0,0 +1,13 @@
+package pl.krusiec.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.krusiec.happyplaces.R
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_detail)
+    }
+}
