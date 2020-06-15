ce21b8600aa9c6079b42a0ce90548ca034819a05
Kamil <krusiec.kamil82@gmail.com>
2020-06-15 12:48:41 +0200

    Reading The Data From Our Local DB

--- app/src/main/java/pl/krusiec/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/pl/krusiec/happyplaces/activities/MainActivity.kt
@@ -3,8 +3,11 @@ package pl.krusiec.happyplaces.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import pl.krusiec.happyplaces.R
+import pl.krusiec.happyplaces.database.DatabaseHandler
+import pl.krusiec.happyplaces.models.HappyPlaceModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
         fabAddHappyPlace.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
+        }
+
+        getHappyPlacesListFromLocalDB()
+    }
+
+    private fun getHappyPlacesListFromLocalDB(){
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
+
+        if (getHappyPlaceList.size > 0){
+            for (i in getHappyPlaceList) {
+                Log.e("Title", i.title)
+                Log.e("Description", i.description)
+            }
         }
     }
 }
