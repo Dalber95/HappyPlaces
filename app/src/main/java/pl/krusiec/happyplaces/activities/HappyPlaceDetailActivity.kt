e7e33b42a4025356a31e912459c39873dc8448a4
Kamil <krusiec.kamil82@gmail.com>
2020-06-15 15:27:18 +0200

    Passing Whole Objects from One Activity To Another

--- app/src/main/java/pl/krusiec/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/pl/krusiec/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -1,13 +1,36 @@
 package pl.krusiec.happyplaces.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_happy_place_detail.*
 import pl.krusiec.happyplaces.R
+import pl.krusiec.happyplaces.models.HappyPlaceModel
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_happy_place_detail)
+
+        var happyPlaceDetailModel: HappyPlaceModel? = null
+
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            happyPlaceDetailModel = intent.getParcelableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel
+        }
+
+        if (happyPlaceDetailModel != null) {
+            setSupportActionBar(toolbarHappyPlaceDetail)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = happyPlaceDetailModel.title
+
+            toolbarHappyPlaceDetail.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            ivPlaceImage.setImageURI(Uri.parse(happyPlaceDetailModel.image))
+            tvDescription.text = happyPlaceDetailModel.description
+            tvLocation.text = happyPlaceDetailModel.location
+        }
     }
 }
