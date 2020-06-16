b5a61eba99dbecf13bbfbcf5ee60f8285aa4aac4
Kamil <krusiec.kamil82@gmail.com>
2020-06-16 17:34:21 +0200

    Adding a Map Intent

--- app/src/main/java/pl/krusiec/happyplaces/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/pl/krusiec/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -1,5 +1,6 @@
 package pl.krusiec.happyplaces.activities
 
+import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,6 +32,12 @@ class HappyPlaceDetailActivity : AppCompatActivity() {
             ivPlaceImage.setImageURI(Uri.parse(happyPlaceDetailModel.image))
             tvDescription.text = happyPlaceDetailModel.description
             tvLocation.text = happyPlaceDetailModel.location
+
+            btnViewOnMap.setOnClickListener {
+                val intent = Intent(this, MapActivity::class.java)
+                intent.putExtra(MainActivity.EXTRA_PLACE_DETAILS, happyPlaceDetailModel)
+                startActivity(intent)
+            }
         }
     }
 }
---
+++ app/src/main/java/pl/krusiec/happyplaces/activities/MapActivity.kt
@@ -0,0 +1,13 @@
+package pl.krusiec.happyplaces.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.krusiec.happyplaces.R
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
